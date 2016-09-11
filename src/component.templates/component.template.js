5952f2ea7e6b14f9deb9ff9138cdaf796f9e4ead
Milen Stanev <milen.stanev@gmail.com>
2016-09-11 19:59:06 +0300

    first commit

---
+++ src/component.templates/component.template.js
@@ -0,0 +1,9 @@
+//region templates
+/* angular-material */
+import componentTemplate from './angular-material/template-material.js';
+
+/* angular-strap */
+//import componentTemplate from './angular-strap/template-bootstrap.js';
+//endregion
+
+export default componentTemplate;
