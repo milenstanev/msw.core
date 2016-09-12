ba0345b5b9f1ac5933817fddabc3192d3e538ad5
Milen Stanev <milen.stanev@gmail.com>
2016-09-12 14:02:57 +0300

    Change

--- src/component.templates/component.template.js
+++ src/component.templates/component.template.js
@@ -1,9 +1,11 @@
-//region templates
+/**
+ * TODO: description, settings .etc
+ */
+
 /* angular-material */
-import componentTemplate from './angular-material/template-material.js';
+//import componentTemplate from './angular-material/template-material.js';
 
 /* angular-strap */
-//import componentTemplate from './angular-strap/template-bootstrap.js';
-//endregion
+import componentTemplate from './angular-strap/template-bootstrap.js';
 
 export default componentTemplate;
