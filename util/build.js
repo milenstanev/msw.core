d194292a1f3da6cdaa96b757b7ea1523ac5731d0
milenstanev <milen.stanev@gmail.com>
2017-09-13 03:16:15 +0300

    new build 2.2

--- util/build.js
+++ util/build.js
@@ -15,26 +15,7 @@ const baseUrl = '.';
 const builder = new Builder(`${baseUrl}/`, `${baseUrl}/config.js`);
 
 builder.config({
-  meta: {
-    'angular': {
-      build: false
-    },
-    'angular-ui/ui-router': {
-      build: false
-    },
-    'angular-sanitize': {
-      build: false
-    },
-    'angular-material': {
-      build: false
-    },
-    'text': {
-      build: false
-    },
-    'css': {
-      build: false
-    }
-  }
+  meta: {}
 });
 
 builder
@@ -44,7 +25,7 @@ builder
     {
       minify: true,
       sourceMaps: true,
-      format: 'cjs', // cjs // es6 without bundle
+      format: 'umd',
       runtime: false
     }
   ).then(function() {
